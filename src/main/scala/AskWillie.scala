e370e65a4e463a7689eec3bfa31f276106d9819a
TheDeafOne <Woodburnkb20@gcc.edu>
2024-04-17 16:03:40 -0400

    timer

--- src/main/scala/AskWillie.scala
+++ src/main/scala/AskWillie.scala
@@ -12,13 +12,14 @@ import scala.util.Sorting
         println("        \\/     \\/     \\/        \\/                       \\/")
         println("=============================================================")
 
+        val t0 = System.nanoTime()
         // Load WebPage.id -> WebPage map to better handle graph
         val pages: Map[String, WebPage] = mapWebPages(loadWebPages()) // completed for you
 
         // TODO: Measure the importance of each page using one of the functions in PageRank
-//        val rankings = PageRank.equal(pages)
+        val rankings = PageRank.equal(pages)
 //        val rankings = PageRank.indegree(pages)
-        val rankings = PageRank.pagerank(pages)
+//        val rankings = PageRank.pagerank(pages)
         println(rankings)
 
         val rankedPages: List[RankedWebPage] = pages.map(p => new RankedWebPage(p._2, rankings.getOrElse(p._1, 0.0))).toList // call PageRank.???? here
@@ -35,14 +36,18 @@ import scala.util.Sorting
             // this is the last line in the expression i.e. the condition of our while loop
             terms != List(":quit")
         } do {
+//          val pageSearchVals = PageSearch.count(rankedPages, terms)
+//          val pageSearchVals = PageSearch.tf(rankedPages, terms)
           val pageSearchVals = PageSearch.tfidf(rankedPages, terms)
-          val searchedPages: List[SearchedWebPage] = rankedPages.zip(pageSearchVals).map(p => new SearchedWebPage(p._1, p._2)).toList // call PageSearch.???? here
+          val searchedPages: List[SearchedWebPage] = rankedPages.zip(pageSearchVals).map(p => new SearchedWebPage(p._1, p._2))
 
           // normalize the ranges for weight and textmatch on these pages
           val pageArray = SearchedWebPageNormalize.normalize(searchedPages).toArray
           // sort this array based on the chosen averaging scheme i.e.
           //    (ArithmeticOrdering || GeometricOrdering || HarmonicOrdering)
           Sorting.quickSort(pageArray)(ArithmeticOrdering)
+          val t1 = System.nanoTime()
+          println("Elapsed time: " + (t1 - t0) + "ns")
           // Print the top ranked pages in descending order
           for p <- pageArray.reverse.slice(0, 10) do println(f"${p.name}%-15s  ${p.url}")
           // print a divider to make reading the results easier
